dd8bae3e271e7d895dbcbc607d7fc4d5cfeb31c8
jaythaceo <jaythaceo@gmail.com>
2014-08-04 00:06:25 -0400

    initial commit

---
+++ src/crawl.py
@@ -0,0 +1,38 @@
+"""
+Web Crawler 1.0
+By: Jason Brooks
+Email: jaythaceo@gmail.com
+
+"""
+import urllib2, cookielib
+import urlparse
+import BeautifulSoup
+import sqlite3
+import time, datetime
+import threading, Queue
+time.clock() # initializing clock
+
+globalData = {
+	'useragent': 'Crawler 1.0',
+	'whitelist': [],
+	'blacklist': [],
+	'startURL': 'http://www.digitalldesk.com/',
+	'threadLimit': 1,
+	'requestInterval': 0.5,
+	'queue': [],
+	'debug':
+}
+
+class Crawl:
+	def __init__(self):
+		if globalData['debug']: print 'Crawl | init called'
+		self.cookie = cookielib.CookieJar()
+		opener = urllib2.build_opener(urllib2.HTTPCookieProcessor(self.cookie))
+		
+
+
+	def getURL(self):
+	
+
+	def requestURL(self):
+		
